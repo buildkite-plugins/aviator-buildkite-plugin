3556f3a76d1839f72a80dc809156518dc7fa113e
Matías A. Bellone <matias.bellone@buildkite.com>
2023-02-18 22:56:02 -0300

    First stab at plugin functionality

---
+++ lib/upload.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+upload() {
+  local API_KEY=$1
+  local FILE_TO_UPLOAD="$2"
+
+  local curl_args=(
+    '-X' 'POST'
+    '--silent'
+    '--show-error'
+    '-H' 'Provider-Name: buildkite-plugin'
+    '-H' "Job-Name: ${BUILDKITE_LABEL}"
+    '-H' "Build-URL: ${BUILDKITE_BUILD_URL}"
+    '-H' "Build-ID: ${BUILDKITE_BUILD_ID}"
+    '-H' "Commit-SHA: ${BUILDKITE_COMMIT}"
+    '-H' "Repo-Url: ${BUILDKITE_REPO}"
+    '-H' "Branch-Name: ${BUILDKITE_BRANCH}"
+    '-H' 'Build-Status": "success"'  # TODO: make this customizable
+    '-d' "@${FILE_TO_UPLOAD}"
+  )
+
+  curl_args+=("${BUILDKITE_PLUGIN_AVIATOR_API_URL:-https://upload.aviator.co/api/test-report-uploader}")
+  
+  curl_args+=("-H" "x-Aviator-Api-Key: '$API_KEY'")
+
+  curl "${curl_args[@]}"
+}
