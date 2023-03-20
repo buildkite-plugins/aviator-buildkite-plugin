854f44c278b411e0d1f47d93005b2372111ac7fd
Matías A. Bellone <matias.bellone@buildkite.com>
2023-03-20 16:07:42 -0300

    Added missing prefix

--- lib/plugin.bash
+++ lib/plugin.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-PLUGIN_PREFIX="YOUR_PLUGIN_NAME"
+PLUGIN_PREFIX="AVIATOR"
 
 # Reads either a value or a list from the given env prefix
 function prefix_read_list() {
